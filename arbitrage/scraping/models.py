11156fa25c9bdcbb0b0257c501f6af6231cd743e
CandideAndKissinger <68018004+CandideAndKissinger@users.noreply.github.com>
2021-11-12 16:29:20 +0000

    User database

--- arbitrage/scraping/models.py
+++ arbitrage/scraping/models.py
@@ -2,12 +2,24 @@ from django.db import models
 
 # Create your models here.
 
-class results(models.Model):
-    user = models.CharField(max_length=64)
+class Result(models.Model):
+    username = models.CharField(max_length=64)
     source_product = models.CharField(max_length=64)
     product = models.CharField(max_length=64)
     retailer = models.CharField(max_length=64)
     url = models.CharField(max_length=128)
     price = models.DecimalField(max_digits=32, decimal_places=8)
     date = models.DateTimeField()
-    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.id}: {self.user}. Product {self.product} for {self.source_product} at {self.price}"
+
+class User(models.Model):
+    username = models.CharField(max_length=64)
+    API_KEY = models.CharField(max_length=64)
+    requests = models.IntegerField()
+    flagged_requests = models.IntegerField()
+    to_be_billed = models.DecimalField(max_digits=32, decimal_places=8)
+
+    def __str__(self):
+        return f"{self.username}"
