7dde2c0659d3b76a6137889ec30084941855fb22
CandideAndKissinger <68018004+CandideAndKissinger@users.noreply.github.com>
2021-11-12 16:05:37 +0000

    Set up results database

--- arbitrage/scraping/models.py
+++ arbitrage/scraping/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class results(models.Model):
+    user = models.CharField(max_length=64)
+    source_product = models.CharField(max_length=64)
+    product = models.CharField(max_length=64)
+    retailer = models.CharField(max_length=64)
+    url = models.CharField(max_length=128)
+    price = models.DecimalField(max_digits=32, decimal_places=8)
+    date = models.DateTimeField()
+    name = models.CharField(max_length=64)
