75464e70d59e4b08a6c8a980686b1579ac354dcd
EthanTheMathmo <eh595@cam.ac.uk>
2021-11-13 23:00:01 +0000

    clearing up ruairidh;s mess (halp plz0

--- arbitrage2/scraping/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("summary", views.summary, name="summary"),
-    path("add_to_blacklist", views.add_to_blacklist, name="add_to_blacklist")
-]
